a0a735c4dbf89cb25ea61700ba7a362f983bd319
Jethro Kuan <jethrokuan95@gmail.com>
2016-03-21 11:03:33 +0800

    Added check for hugo in path

--- functions/__hugo_deploy.fish
+++ functions/__hugo_deploy.fish
@@ -2,24 +2,29 @@ function __hugo_deploy -d " Deploy script"
   set -l date (date)
   set -l HUGO_SOURCE_MSG "rebuilding site source ($date)"
   set -l HUGO_PUBLIC_MSG "rebuilding public site ($date)"
+
+  if not type -q hugo
+    printf "`hugo` command not found. Please check your $PATH."
+    return 1
+  end
   
   hugo > /dev/null; and pushd public
 
   if test ! -s CNAME
     get -p "Enter the url for your site (eg. blog.example.com): " | read cname
-    echo "Adding CNAME to public folder..."
-    echo $cname > CNAME
-  end
+      echo "Adding CNAME to public folder..."
+      echo $cname > CNAME
+    end
 
-  if git_is_dirty
-    git add -A; and git commit -m "$HUGO_SOURCE_MSG" > /dev/null; and git push origin master --quiet
-  end
+    if git_is_dirty
+      git add -A; and git commit -m "$HUGO_SOURCE_MSG" > /dev/null; and git push origin master --quiet
+    end
   
-  popd
+    popd
 
-  if git_is_dirty
-    git add -A; and git commit -m "$HUGO_PUBLIC_MSG" > /dev/null; and git push origin source --quiet
-  end
+    if git_is_dirty
+      git add -A; and git commit -m "$HUGO_PUBLIC_MSG" > /dev/null; and git push origin source --quiet
+    end
 
-  return 0
-end  
+    return 0
+  end  
